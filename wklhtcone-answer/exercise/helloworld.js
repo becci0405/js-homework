424c7e49bea3851b621db7585b20d1f81a47e237
becci0405 <becci0405@sohu.com>
2017-07-31 22:05:48 +0800

    Update helloworld.js

--- wklhtcone-answer/exercise/helloworld.js
+++ wklhtcone-answer/exercise/helloworld.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <script type="text/javascript">
+        	console.log("Hello world!");
+        </script>
+    </head>
+    <body>
+    	
+    </body>
+</html>
